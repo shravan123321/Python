0ac7cdd4eea1232412a90ce5ebad82fbe3b43fc8
Shravan Kumar <201530770+shravan123321@users.noreply.github.com>
2025-10-16 13:06:36 +0530

    Delete Exception_hindling directory

--- Exception_hindling/01_basic_exception.py
+++
@@ -1,7 +0,0 @@
-# seting the string value stop Exception throw
-str = "shravan kumar "
-for s in str:
-    if s != o :
-        print(s)
-print("hello ji")
-print("l will not excuted for the code")
--- Exception_hindling/02_handel_exceptio.py
+++
@@ -1,8 +0,0 @@
-# Handel the exception 
-arr = ["mango","apple","orange"]
-try:
-    for i in range(5) :
-        print(arr[i])
-except :
-    print("Exception Handled ")
-print("this line has to be mandatorily exception ")
